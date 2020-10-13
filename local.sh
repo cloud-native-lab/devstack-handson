1f885f57b606c1061e7afd3150e6b89be484d332
makocchi-git <makocchi@gmail.com>
2020-10-13 12:29:15 +0900

    add sudo

--- local.sh
+++ local.sh
@@ -7,4 +7,4 @@ source $TOP_DIR/openrc
 openstack subnet set --dns-nameserver 8.8.8.8 private-subnet
 
 # for cinder volume
-losetup -f --show /opt/stack/data/stack-volumes-lvmdriver-1-backing-file
+sudo losetup -f --show /opt/stack/data/stack-volumes-lvmdriver-1-backing-file
